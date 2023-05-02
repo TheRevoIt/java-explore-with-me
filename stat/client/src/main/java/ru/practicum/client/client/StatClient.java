0070a2b29fe0167c2f1dfc1bb30c86854559621b
Vladimir Veretennikov <revosierra@gmail.com>
2023-05-02 21:21:21 +0200

    feat: added stat client

---
+++ stat/client/src/main/java/ru/practicum/client/client/StatClient.java
@@ -0,0 +1,47 @@
+package ru.practicum.client.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.statdto.dto.HitDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class StatClient extends BaseClient {
+    private static final String HIT_PATH = "/hit";
+    private static final String STATS_PATH = "/stats";
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Autowired
+    public StatClient(@Value("${server.url}") String url, RestTemplateBuilder builder) {
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(url))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new).build());
+    }
+
+    public ResponseEntity<Object> save(HitDto hitDto) {
+        return post(HIT_PATH, hitDto);
+    }
+
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, boolean unique, List<String> urls) {
+            String paramsUri = urls.stream().reduce("", (a, b) -> b + "," + a);
+
+        Map<String, Object> parameters = Map.of(
+                "start", start.format(DATE_TIME_FORMATTER),
+                "end", end.format(DATE_TIME_FORMATTER),
+                "uris", paramsUri,
+                "unique", unique);
+
+        return get(STATS_PATH + "?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+    }
+}
