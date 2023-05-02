50a88c4be0ac3dcec7ebd218a78c741911112fc1
Vladimir Veretennikov <revosierra@gmail.com>
2023-05-02 21:14:57 +0200

    feat: fixed modules structures

--- stat/stat-service/src/main/java/ru/practicum/stat/controller/StatController.java
+++ stat/stat-service/src/main/java/ru/practicum/stat/controller/StatController.java
@@ -2,14 +2,15 @@ package ru.practicum.stat.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.stat.dto.HitDto;
-import ru.practicum.stat.dto.ViewStatsDto;
 import ru.practicum.stat.service.StatService;
+import ru.practicum.statdto.dto.HitDto;
+import ru.practicum.statdto.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,14 +23,16 @@ public class StatController {
 
     @PostMapping("/hit")
     HitDto save(@RequestBody HitDto hitDto) {
+        log.info("Perform saving");
         return statService.save(hitDto);
     }
 
     @GetMapping("/stats")
-    ViewStatsDto getStats(@RequestParam LocalDateTime start,
-                          @RequestParam LocalDateTime end,
-                          @RequestParam(defaultValue = "false") boolean unique,
-                          @RequestParam(required = false) List<String> urls) {
+    List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                @RequestParam(defaultValue = "false") boolean unique,
+                                @RequestParam(name = "uris", required = false) List<String> urls) {
+        log.info("Get stats");
         return statService.getStats(start, end, unique, urls);
     }
 }
